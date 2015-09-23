dfd82dfbb7a4a37260bcf19147cce3625910e7d3
Chumilion <2016esison@tjhsst.edu>
2015-09-23 11:32:45 -0400

    SharedPreferences done!

--- app/src/main/java/chumilion/buttontoasts/MainActivity.java
+++ app/src/main/java/chumilion/buttontoasts/MainActivity.java
@@ -1,5 +1,7 @@
 package chumilion.buttontoasts;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,20 +14,28 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
-
-    Integer[] textPresses = {0, 0, 0, 0};
+    Integer[] textPresses = new Integer[4];
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences mySP = getPreferences(Context.MODE_PRIVATE);
+        final SharedPreferences.Editor mySPEditor = mySP.edit();
 
 
         TextView[] textViews = {(TextView) findViewById(R.id.tl_textView),
                                 (TextView) findViewById(R.id.tr_textView),
                                 (TextView) findViewById(R.id.bl_textView),
                                 (TextView) findViewById(R.id.br_textView)};
+
+        for(int x = 0; x < 4; x++)
+        {
+            textPresses[x] = mySP.getInt("textViews_" + x, 0);
+        }
 
 
         for(int x = 0; x < textViews.length; x++)
@@ -45,11 +55,30 @@ public class MainActivity extends AppCompatActivity
                         timez = " times.";
                     Toast.makeText(MainActivity.this, "Pressed " + textPresses[y] + timez,
                             Toast.LENGTH_SHORT).show();
-                    Log.i("onCreate", textViewz[y].getId() + " was pressed " +
+                    Log.i("onCreate", textViewz[y].getText() + " was pressed " +
                             textPresses[y] + timez);
+                    mySPEditor.putInt("textViews_" + y, textPresses[y]);
+                    mySPEditor.commit();
                 }
             });
         }
+
+        TextView res_textView = (TextView) findViewById(R.id.res_textView);
+        res_textView.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                Toast.makeText(MainActivity.this, "Resetting press count...",
+                        Toast.LENGTH_SHORT).show();
+                Log.i("onCreate", "Resetting press count...");
+                for(int x = 0; x < 4; x++)
+                {
+                    mySPEditor.putInt("textViews_" + x, 0);
+                    textPresses[x] = 0;
+                }
+                mySPEditor.commit();
+            }
+        });
 
     }
 
